03513e380cee00661ad6dd65eb8dcdddcb1ec387
GOM <my21081@gmail.com>
2019-07-05 11:38:55 +0700

    update scripts

--- index.js
+++ index.js
@@ -3,16 +3,20 @@ const { JSDOM } = require('jsdom');
 const file = fs.readFileSync('./index.html', { encoding: "utf8" });
 
 // convert file to DOM
-var dom = new JSDOM(file);
-var document = dom.window.document;
-
-// Append to api id
-const api = document.getElementById('api');
-api.appendChild(div);
+const dom = new JSDOM(file);
+const document = dom.window.document;
 
 // Simple append child
 const div = document.createElement("div");
 div.innerHTML = "A div";
+div.classList = "class";
 document.body.appendChild(div);
 
-fs.writeFileSync('./index.html', document.documentElement.outerHTML , { encoding: "utf8" });
+// Append to api id
+const api = document.getElementById('api');
+const div2 = document.createElement("div");
+div2.innerHTML = "A div";
+div2.classList = "class";
+api.appendChild(div2);
+
+fs.writeFileSync('./index.html', dom.serialize() , { encoding: "utf8" });
