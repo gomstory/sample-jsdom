a5d32e6a1440b54520c1abb82073e3eec14d4983
GOM <my21081@gmail.com>
2019-07-05 10:43:14 +0700

    pretty file

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const fs = require('fs');
 const { JSDOM } = require('jsdom');
-
 const file = fs.readFileSync('./index.html', { encoding: "utf8" });
+
+// convert file to DOM
 var dom = new JSDOM(file);
 var document = dom.window.document;
 
+// Append to api id
+const api = document.getElementById('api');
+api.appendChild(div);
+
+// Simple append child
 const div = document.createElement("div");
 div.innerHTML = "A div";
 document.body.appendChild(div);
